53fc3836e5f32a070ac74c5c7656d0ae6974f98f
defeaz <gus.ferranti@gmail.com>
2025-05-14 21:16:16 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, Mechanic
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    query = request.args.get('location', '')
+    if query:
+        mechanics = Mechanic.query.filter(Mechanic.location.ilike(f"%{query}%")).all()
+    else:
+        mechanics = Mechanic.query.all()
+    return render_template('index.html', mechanics=mechanics)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        m = Mechanic(
+            name=request.form['name'],
+            location=request.form['location'],
+            services=request.form['services'],
+            contact=request.form['contact']
+        )
+        db.session.add(m)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('register.html')
+
+@app.route('/mechanic/<int:mechanic_id>')
+def mechanic_detail(mechanic_id):
+    mechanic = Mechanic.query.get_or_404(mechanic_id)
+    return render_template('mechanic_detail.html', mechanic=mechanic)
